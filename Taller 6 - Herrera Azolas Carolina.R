781faff944eeb39dec4441c428557923e70e43d0
CarolinsRainbow <60187454+Carolinsrainbow@users.noreply.github.com>
2021-07-30 06:14:01 -0400

    Add files via upload

---
+++ Taller 6 - Herrera Azolas Carolina.R
@@ -0,0 +1,12 @@
+## Taller 6 - Minería de datos
+## Carolina Herrera Azolas
+
+## Incorporación de datos 
+datos <- read.csv("/Users/user/Desktop/DatosPaises.csv",sep=";", header=TRUE)
+
+## Visualización general de datos
+head(datos)
+
+## Resumen estadístico 
+summary(datos)
+
