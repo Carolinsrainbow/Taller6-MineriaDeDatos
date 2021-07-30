6b4b9e73f1e9f5b1d402dd3c4d89db476b959e71
CarolinsRainbow <60187454+Carolinsrainbow@users.noreply.github.com>
2021-07-30 06:36:57 -0400

    Add files via upload

--- Taller 6 - Herrera Azolas Carolina.R
+++ Taller 6 - Herrera Azolas Carolina.R
@@ -10,3 +10,8 @@ head(datos)
 ## Resumen estadístico 
 summary(datos)
 
+## Creamos un vector con 28 columnas, sin considerar país (variable string )
+cor(datos[c(2:30)])
+
+##Unimos la matriz creada relacionandola con el PIB
+cor(datos$PIB,datos[c(3:30)])
